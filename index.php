d569fbe17e7d0fbbf15e33d2e1232cda55c4ec9e
Marina A <contact@tentenbiz.com>
2018-11-05 12:50:17 +0800

    Create index.php
    The main file.

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: WP TinyMCE Custom Style
+Plugin URI: https://tentenbiz.com
+Description: TinyMCE custom style.
+Author: Tentenbiz Solutions
+Version: 0.1
+*/
+
+function ttb_custom_tinymce_style( $in ) {
+	$in['content_css'] = plugin_dir_url( __FILE__ ) . "/tinymce-style.css";
+	return $in;
+}
+add_filter( 'tiny_mce_before_init', 'ttb_custom_tinymce_style' );
+?>
